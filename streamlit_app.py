e9bed5646493b0e59c37c20f8131ec1eff674224
squaretec <jorge@att.net.mx>
2024-09-19 12:10:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ def get_gdp_data():
     raw_gdp_df = pd.read_csv(DATA_FILENAME)
 
     MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    MAX_YEAR = 1995
 
     # The data above has columns like:
     # - Country Name
